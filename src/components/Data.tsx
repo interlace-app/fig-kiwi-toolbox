e6bae4d3d3e6611ad8e6305fef7afe1f54f2aafe
Ricky Zhang <haoyuzhangca@outlook.com>
2023-11-15 15:57:53 -0500

    make data max height longer

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -5,7 +5,7 @@ export interface DataProps extends React.HTMLAttributes<HTMLDivElement> {}
 export const Data = ({ className, children, ...props }: DataProps) => (
   <div
     className={twMerge(
-      'flex px-3 py-2 overflow-y-auto text-sm rounded-md max-h-80 text-slate-700 bg-slate-100  break-words select-text cursor-text nodrag',
+      'flex px-3 py-2 overflow-y-auto text-sm rounded-md max-h-96 text-slate-700 bg-slate-100  break-words select-text cursor-text nodrag',
       className,
     )}
     {...props}
